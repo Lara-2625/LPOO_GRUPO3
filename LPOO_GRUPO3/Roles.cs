b6f1ccfc6832dd2eff11e6adda364717df5adb97
DESKTOP-9UJEHGC\maxi <nahuel_maxi11@hotmail.com>
2022-06-16 04:54:08 -0300

    Corecciones BD

---
+++ LPOO_GRUPO3/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LPOO_GRUPO3
+{
+    public class Roles
+    {
+        private int rol_Codigo;
+        private string rol_Descripcion;
+    }
+}
---
+++ LPOO_GRUPO3/Usuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LPOO_GRUPO3
+{
+    public class Usuario
+    {
+        private int usu_ID;
+        private string usu_NobreUsuario;
+        private string usu_Contraseña;
+        private string usu_ApellidoNombre;
+        private int rol_Codigo;
+
+        public string Usu_Contraseña
+        {
+            get { return usu_Contraseña; }
+            set { usu_Contraseña = value; }
+        }
+        public int Usu_ID
+        {
+            get { return usu_ID; }
+            set { usu_ID = value; }
+        }
+        public string Usu_NombreUsuario
+        {
+            get { return usu_NobreUsuario; }
+            set { usu_NobreUsuario = value; }
+        }
+        public string UsuApellidoNombre
+        {
+            get { return usu_ApellidoNombre; }
+            set { usu_ApellidoNombre = value; }
+        }
+        public int Rol_Codigo
+        {
+            get { return Rol_Codigo; }
+            set { Rol_Codigo = value; }
+        }
+
+        public Usuario(string username, string password)
+        {
+            usu_NobreUsuario = username;
+            usu_Contraseña = password;
+        }
+    
+    }
+}
